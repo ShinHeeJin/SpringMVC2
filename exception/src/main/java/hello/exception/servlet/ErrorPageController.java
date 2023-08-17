7703787673472528df067cd24fc252543fb944f9
ShinHeeJin <sisesu92@gmail.com>
2023-08-18 02:50:02 +0900

    fix minor

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -12,20 +12,20 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class ErrorPageController {
 
-    @GetMapping("/error-page/404")
+    @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 404");
         printErrorInfo(request);
-        return "error-page/404";
+        return "/error-page/404";
     }
 
     private void printErrorInfo(HttpServletRequest request) {
-        log.info("ERROR info : {}", request.getAttribute("javax.servlet.error.exception"));
-        log.info("ERROR info : {}", request.getAttribute("javax.servlet.error.exception_type"));
-        log.info("ERROR info : {}", request.getAttribute("javax.servlet.error.message"));
-        log.info("ERROR info : {}", request.getAttribute("javax.servlet.error.request_uri"));
-        log.info("ERROR info : {}", request.getAttribute("javax.servlet.error.servlet_name"));
-        log.info("ERROR info : {}", request.getAttribute("javax.servlet.error.status_code"));
+        log.info("ERROR exception : {}", request.getAttribute("javax.servlet.error.exception"));
+        log.info("ERROR exception_type : {}", request.getAttribute("javax.servlet.error.exception_type"));
+        log.info("ERROR message : {}", request.getAttribute("javax.servlet.error.message"));
+        log.info("ERROR request_uri : {}", request.getAttribute("javax.servlet.error.request_uri"));
+        log.info("ERROR servlet_name : {}", request.getAttribute("javax.servlet.error.servlet_name"));
+        log.info("ERROR status_code : {}", request.getAttribute("javax.servlet.error.status_code"));
     }
 
     @RequestMapping("/error-page/500")
