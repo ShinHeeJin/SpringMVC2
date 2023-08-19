7a2f993633ca1d4db1e0cbfd10f5afd1aaf36490
ShinHeeJin <sisesu92@gmail.com>
2023-08-19 12:52:33 +0900

    ApiException Handle

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,12 +1,21 @@
 package hello.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+import static javax.servlet.RequestDispatcher.ERROR_EXCEPTION;
+import static javax.servlet.RequestDispatcher.ERROR_STATUS_CODE;
 
 @Slf4j
 @Controller
@@ -20,12 +29,12 @@ public class ErrorPageController {
     }
 
     private void printErrorInfo(HttpServletRequest request) {
-        log.info("ERROR exception : {}", request.getAttribute("javax.servlet.error.exception"));
+        log.info("ERROR exception : {}", request.getAttribute(ERROR_EXCEPTION));
         log.info("ERROR exception_type : {}", request.getAttribute("javax.servlet.error.exception_type"));
         log.info("ERROR message : {}", request.getAttribute("javax.servlet.error.message"));
         log.info("ERROR request_uri : {}", request.getAttribute("javax.servlet.error.request_uri"));
         log.info("ERROR servlet_name : {}", request.getAttribute("javax.servlet.error.servlet_name"));
-        log.info("ERROR status_code : {}", request.getAttribute("javax.servlet.error.status_code"));
+        log.info("ERROR status_code : {}", request.getAttribute(ERROR_STATUS_CODE));
     }
 
     @RequestMapping("/error-page/500")
@@ -35,4 +44,17 @@ public class ErrorPageController {
         return "error-page/500";
     }
 
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response) {
+        log.info("API errorPage 500");
+
+        Map<String, Object> result = new HashMap<>();
+
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(ERROR_STATUS_CODE);
+        return new ResponseEntity(result, HttpStatus.valueOf(statusCode));
+    }
 }
