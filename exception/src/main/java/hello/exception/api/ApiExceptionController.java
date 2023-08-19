09c43f2512a67a66b55e7bf4a1222d2ade549664
ShinHeeJin <sisesu92@gmail.com>
2023-08-19 18:02:08 +0900

    add ResponseStatus( code, reason ) Exception

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,5 +1,6 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,9 +30,14 @@ public class ApiExceptionController {
         return new MemberDTO(id, "hello" + id);
     }
 
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
     @Data
     @AllArgsConstructor
-    static class MemberDTO{
+    static class MemberDTO {
         private String memberId;
         private String name;
     }
