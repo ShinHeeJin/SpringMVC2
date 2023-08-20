1bcbf9d43436800de2cb192e973f5fec3340c527
ShinHeeJin <sisesu92@gmail.com>
2023-08-20 17:17:39 +0900

    ControllerAdvice

--- exception/src/main/java/hello/exception/exhandler/ApiExceptionV2Controller.java
+++ exception/src/main/java/hello/exception/exhandler/ApiExceptionV2Controller.java
@@ -13,27 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ApiExceptionV2Controller {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandle(IllegalArgumentException e) {
-        log.error("[exceptionHandle] ex", e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(Exception.class)
-    public ErrorResult exHandle(Exception e) {
-        log.error("[exceptionHandle] ex", e);
-        return new ErrorResult("EX", "내부 오류");
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> userExHandle(UserException e) {
-        log.error("[exceptionHandle] ex", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
     @GetMapping("/api2/members/{id}")
     public MemberDTO getMember(@PathVariable("id") String id) {
         if (id.equals("ex")) {
